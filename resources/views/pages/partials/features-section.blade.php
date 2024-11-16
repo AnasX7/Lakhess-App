fce51c82f8e10b2ab43fd027a9d15323eafe4884
haneen <bashweehh@gmail.com>
2024-11-16 08:33:23 +0300

    Add features sections

---
+++ resources/views/pages/partials/features-section.blade.php
@@ -0,0 +1,225 @@
+<section class="flex flex-col w-full gap-16 bg-white pb-7 pt-7 md:pb-24 md:pt-20">
+
+    <div class="flex flex-col items-center gap-2 px-20">
+        <h2 class="text-xl font-bold text-primary">Features</h2>
+        <h3 class="text-4xl font-bold text-fg-primary">AI-Powered Summaries and Quizzes for Effortless Learning</h3>
+        <div class="px-20 pt-2 mx-6 text-xl text-center text-fg-tertiary w-15px">
+            <p>Streamline your study sessions with powerful AI-based tools that summarize your lectures and
+                create quizzes to reinforce learning. Trusted by students and professionals for efficient content
+                organization
+                and review </p>
+        </div>
+
+    </div>
+
+    <div
+        class="box-border flex flex-col items-center content-center mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row md:justify-center">
+
+        <div class="relative w-full px-4 mt-5 mb-4 -ml-5 md:ml-0 md:mt-0 md:w-3/4 lg:mb-0 lg:w-2/3 xl:w-1/2 xl:pl-10">
+            <img src="{{ asset('assets/lakhess-app.test_profile.png') }}"
+                class="w-full h-auto border border-gray-200 shadow-lg rounded-xl" alt="App Screenshot" />
+        </div>
+
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:order-none md:w-1/2 md:pl-10">
+            <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300">
+                Organize with Custom Folders
+            </h2>
+            <p
+                class="pt-4 pb-8 m-0 text-lg leading-7 text-gray-700 border-0 border-gray-300 font-regular sm:pr-12 xl:pr-32">
+                Whether studying multiple subjects or deep-diving into a single topic, custom folders make it easy to
+                keep your content organized and accessible.
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span> Create
+                    topic-specific folders to organize summaries and quizzes.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Group related
+                    lectures in one place for a streamlined study experience
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Easily find and
+                    access all summaries and quizzes for a specific topic.
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+    </div>
+
+    <div
+        class="box-border flex flex-col items-center content-center mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row">
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:order-none md:w-1/2 md:pl-10">
+            <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300">
+                Generate Summaries Quizzes Instantly
+            </h2>
+            <p
+                class="pt-4 pb-8 m-0 text-lg leading-7 text-gray-700 border-0 border-gray-300 font-regular sm:pr-12 xl:pr-32">
+                Generate detailed summaries and customized quizzes directly from your lecture PDFs using AI.
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Turn dense content into concise summaries to make
+                    studying
+                    faster.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Create quizzes that challenge your understanding
+                    and help you
+                    retain knowledge.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Access summaries and quizzes instantly after
+                    generation.
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+
+        <!-- Image -->
+        <div class="relative w-full px-4 mt-5 mb-4 -ml-5 md:w-3/4 lg:w-2/3 xl:w-1/2 md:ml-0 md:mt-0 lg:mb-0 xl:pl-10">
+            <img src="{{ asset('assets/lakhess-app.test_profile.png') }}"
+                class="w-full h-auto border border-gray-200 shadow-lg rounded-xl" alt="App Screenshot" />
+        </div>
+    </div>
+
+    <div
+        class="box-border flex flex-col items-center content-center mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row">
+
+        <!-- Image -->
+        <div class="relative w-full px-4 mt-5 mb-4 -ml-5 md:w-3/4 lg:w-2/3 xl:w-1/2 md:ml-0 md:mt-0 lg:mb-0 xl:pl-10">
+            <img src="{{ asset('assets/lakhess-app.test_profile.png') }}"
+                class="w-full h-auto border border-gray-200 shadow-lg rounded-xl" alt="App Screenshot" />
+        </div>
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:order-none md:w-1/2 md:pl-10">
+            <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300">
+                Mark Your Favorites
+            </h2>
+            <p
+                class="pt-4 pb-8 m-0 text-lg leading-7 text-gray-700 border-0 border-gray-300 font-regular sm:pr-12 xl:pr-32">
+                Save and quickly access your most important summaries and quizzes by marking them as favorites.
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Prioritize key materials with favorites.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Access favorite items in one dedicated folder.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Keep track of essential summaries and quizzes for
+                    quick revision.
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+    </div>
+
+    <div
+        class="box-border flex flex-col items-center content-center mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row">
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:order-none md:w-1/2 md:pl-10">
+            <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300">
+                Special Folders for Easy Access
+            </h2>
+            <p
+                class="pt-4 pb-8 m-0 text-lg leading-7 text-gray-700 border-0 border-gray-300 font-regular sm:pr-12 xl:pr-32">
+                Use specialized folders like "View All" and "Favorite" for quick access to all your learning materials.
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>“View All” lets you see every summary and quiz
+                    across all
+                    folders.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>"Favorite" saves your marked items for quick
+                    access.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Navigate seamlessly through all your study content.
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+
+        <!-- Image -->
+        <div class="relative w-full px-4 mt-5 mb-4 -ml-5 md:w-3/4 lg:w-2/3 xl:w-1/2 md:ml-0 md:mt-0 lg:mb-0 xl:pl-10">
+            <img src="{{ asset('assets/lakhess-app.test_profile.png') }}"
+                class="w-full h-auto border border-gray-200 shadow-lg rounded-xl" alt="App Screenshot" />
+        </div>
+    </div>
+
+    <div
+        class="box-border flex flex-col items-center content-center mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row">
+
+        <!-- Image -->
+        <div class="relative w-full px-4 mt-5 mb-4 -ml-5 md:w-3/4 lg:w-2/3 xl:w-1/2 md:ml-0 md:mt-0 lg:mb-0 xl:pl-10">
+            <img src="{{ asset('assets/lakhess-app.test_profile.png') }}"
+                class="w-full h-auto border border-gray-200 shadow-lg rounded-xl" alt="App Screenshot" />
+        </div>
+
+        <!-- Content -->
+        <div class="box-border order-first w-full text-black border-solid md:order-none md:w-1/2 md:pl-10">
+            <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300">
+                User Dashboard for Effortless Navigation
+            </h2>
+            <p
+                class="pt-4 pb-8 m-0 text-lg leading-7 text-gray-700 border-0 border-gray-300 font-regular sm:pr-12 lg:text-lg xl:pr-32">
+                Stay organized with a personalized dashboard that keeps all your materials at your
+            </p>
+            <ul class="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span> View recent activity and easily locate your
+                    latest summaries and quizzes.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Manage folders, favorites, and account settings
+                    from one central place.
+                </li>
+                <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
+                    <span
+                        class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white rounded-full bg-bg-primary-dark"><span
+                            class="text-sm font-bold">✓</span></span>Access everything you need to streamline your
+                    study experience.
+                </li>
+            </ul>
+        </div>
+        <!-- End  Content -->
+    </div>
+</section>
