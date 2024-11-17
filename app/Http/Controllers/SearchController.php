9ecbb629f245ba36ea203648df033586e33ccde1
AhmedSawx <50724805+AhmedSawx@users.noreply.github.com>
2024-11-18 00:46:09 +0300

    added counter for folder

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,16 +11,20 @@ class SearchController extends Controller
         // Initialize arrays to hold the results
         $summaries = [];
         $quizzes = [];
+        $total_folder_count = [];
 
         switch ($category) {
             case '':
                 foreach (auth()->user()->folders as $folder) {
+                    $total_folder_count[$folder->id] = 0;
                     $folderSummaries = $folder->searchsummaries($search)->get();
                     foreach ($folderSummaries as $summary) {
                         array_push($summaries, $summary);
                         $quizzesForSummary = $summary->searchQuiz($search)->get();
+                        $total_folder_count[$folder->id]++;
                         foreach ($quizzesForSummary as $quiz) {
                             array_push($quizzes, $quiz);
+                            $total_folder_count[$folder->id]++;
                         }
                     }
                 }
@@ -28,17 +32,21 @@ class SearchController extends Controller
 
             case 'summary':
                 foreach (auth()->user()->folders as $folder) {
+                    $total_folder_count[$folder->id] = 0;
                     foreach ($folder->searchsummaries($search)->get() as $summary) {
                         array_push($summaries, $summary);
+                        $total_folder_count[$folder->id]++;
                     }
                 }
                 break;
 
             case 'quiz':
                 foreach (auth()->user()->folders as $folder) {
+                    $total_folder_count[$folder->id] = 0;
                     foreach ($folder->summaries as $summary) {
                         foreach ($summary->searchQuiz($search)->get() as $quiz) {
                             array_push($quizzes, $quiz);
+                            $total_folder_count[$folder->id]++;
                         }
                     }
                 }
@@ -55,6 +63,7 @@ class SearchController extends Controller
         //We will store these in a session since we need to use it on the side-nav
         session()->flash('summaries', $summaries);
         session()->flash('quizzes', $quizzes);
+        session()->flash('total', $total_folder_count);
 
         return view('app.search', compact('summaries', 'quizzes', 'category'));
     }
