2a0e2554f0679d5612ccd764d8c9c8306a8d391b
AhmedSawx <50724805+AhmedSawx@users.noreply.github.com>
2024-11-18 09:16:24 +0300

    changed search to only summaries

--- resources/views/app/search.blade.php
+++ resources/views/app/search.blade.php
@@ -4,51 +4,13 @@
     {{-- TESTING PURPOSES!! CHANGE LATER!! --}}
     <div class="p-4">
         <div class="flex flex-wrap gap-4">
-            @switch($category)
-                @case('')
-                    {{-- We do both --}}
-                    @if (empty($summaries) && empty($quizzes))
-                        <p class="justify-center text-xl">Nothing here...</p>
-                    @else
-                        @foreach ($summaries as $summary)
-                            @include('app.shared.summary-card')
-                        @endforeach
-
-                        @foreach ($quizzes as $quiz)
-                            @include('app.shared.quiz-card')
-                        @endforeach
-                    @endif
-                @break
-
-                @case('summary')
-                    {{-- Show summaries only --}}
-                    @if (empty($summaries))
-                        <p class="justify-center text-xl">Nothing here...</p>
-                    @else
-                        @foreach ($summaries as $summary)
-                            @include('app.shared.summary-card')
-                        @endforeach
-                    @endif
-                @break
-
-                @case('quiz')
-                    {{-- Show quizzes only --}}
-
-                    @if (empty($quizzes))
-                        <p class="justify-center text-xl">Nothing here...</p>
-                    @else
-                        @foreach ($quizzes as $quiz)
-                            @include('app.shared.quiz-card')
-                        @endforeach
-                    @endif
-                @break
-
-                @default
-                    <p class="justify-center text-xl">Nothing here...</p>
-                @break
-
-            @endswitch
-
+            @if (empty($summaries))
+                <p class="justify-center text-xl">Nothing here...</p>
+            @else
+                @foreach ($summaries as $summary)
+                    @include('app.shared.summary-card')
+                @endforeach
+            @endif
         </div>
     </div>
 </x-app-layout>
