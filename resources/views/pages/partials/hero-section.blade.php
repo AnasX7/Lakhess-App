ea13048a9c015fb641591cc6da8143c49edbf786
Anas Salem <anassalem.aa@gmail.com>
2024-11-19 14:31:08 +0400

    fixs in langing page

--- resources/views/pages/partials/hero-section.blade.php
+++ resources/views/pages/partials/hero-section.blade.php
@@ -1,20 +1,26 @@
-<div id="grid" class="hero pt-[8rem] flex w-full flex-col items-center gap-[48px] bg-bg-primary">
- 
-    <div class="flex flex-col items-center gap-[32px]">
-      <h1 class="text-[60px] font-semibold text-fg-primary">Smart Study with AI Summaries & Quizzes</h1>
-      <p class="font-regular text-center text-[20px] text-fg-tertiary">
-        Quickly summarize lecture and create quizzes with power of AI<br>
-        everything you need to study smarter and faster⚡
-      </p>
-    </div>
+<div id="grid" class="hero flex w-full flex-col items-center gap-[48px] bg-bg-primary pt-[8rem]">
 
-    <div class="flex flex-col items-center gap-[60px]">
-      <a class="btn rounded-[10px] border-[2px] border-primary bg-primary text-[18px] font-semibold text-[#FFFFFF] hover:border-primary hover:bg-secondary"
-        href="#">get started for free</a>
-
-      <img class="max-w-5xl h-auto border border-gray-200 shadow-lg rounded-xl "
-        src="{{asset("assets/lakhess-app.test_dashboard.png")}}"
-        alt="imge not suport its Brouser">
-    </div>
-
+  <div class="flex flex-col items-center gap-[32px]">
+    <h1 class="text-[60px] font-semibold text-fg-primary">Smart Study with AI Summaries & Quizzes</h1>
+    <p class="font-regular text-center text-[20px] text-fg-tertiary">
+      Quickly summarize lecture and create quizzes with power of AI<br>
+      everything you need to study smarter and faster⚡
+    </p>
   </div>
+
+  <div class="flex flex-col items-center gap-[60px]">
+    @auth
+      <a class="btn rounded-[10px] border-[2px] border-primary bg-primary text-[18px] font-semibold text-[#FFFFFF] hover:border-primary hover:bg-secondary"
+        href="{{ route('dashboard') }}">get started for free</a>
+    @endauth
+
+    @guest
+      <a class="btn rounded-[10px] border-[2px] border-primary bg-primary text-[18px] font-semibold text-[#FFFFFF] hover:border-primary hover:bg-secondary"
+        href="{{ route('login') }}">get started for free</a>
+    @endguest
+
+    <img class="h-auto max-w-5xl rounded-xl border border-gray-200 shadow-lg"
+      src="{{ asset('assets/lakhess-app.test_dashboard.png') }}" alt="imge not suport its Brouser">
+  </div>
+
+</div>
